b055a3f2edff147e9074d490885e821775268fec
Diefonro <dfonsecar@unal.edu.co>
2021-10-14 23:03:52 -0500

    Changed the text shadow on the title

--- calculator/app.js
+++ calculator/app.js
@@ -1,66 +1,68 @@
 function add() {
-    const form = document.getElementById("form");
-    
-    let operandA = form['operandA'];
-    let operandB = form['operandB'];
-    
-    let resultAdd = parseInt(operandA.value) + parseInt(operandB.value);
-    if (isNaN(resultAdd)) {
-        resultAdd = "The operation does not include numbers."
-    } 
-    document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
+  const form = document.getElementById("form");
 
+  let operandA = form["operandA"];
+  let operandB = form["operandB"];
+
+  let resultAdd = parseInt(operandA.value) + parseInt(operandB.value);
+  if (isNaN(resultAdd)) {
+    resultAdd = "The operation does not include numbers.";
+  }
+  document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
 }
 
 function subs() {
-    const form = document.getElementById("form");
-    
-    let operandA = form['operandA'];
-    let operandB = form['operandB'];
-    
-    let resultAdd = parseInt(operandA.value) - parseInt(operandB.value);
-    if (isNaN(resultAdd)) {
-        resultAdd = "The operation does not include numbers."
-    } 
-    document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
+  const form = document.getElementById("form");
+
+  let operandA = form["operandA"];
+  let operandB = form["operandB"];
+
+  let resultAdd = parseInt(operandA.value) - parseInt(operandB.value);
+  if (isNaN(resultAdd)) {
+    resultAdd = "The operation does not include numbers.";
+  }
+  document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
 }
 
 function mult() {
-    const form = document.getElementById("form");
-    
-    let operandA = form['operandA'];
-    let operandB = form['operandB'];
-    
-    let resultAdd = parseInt(operandA.value) * parseInt(operandB.value);
-    if (isNaN(resultAdd)) {
-        resultAdd = "The operation does not include numbers."
-    } 
-    document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
+  const form = document.getElementById("form");
+
+  let operandA = form["operandA"];
+  let operandB = form["operandB"];
+
+  let resultAdd = parseInt(operandA.value) * parseInt(operandB.value);
+  if (isNaN(resultAdd)) {
+    resultAdd = "The operation does not include numbers.";
+  }
+  document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
 }
 
 function divd() {
-    const form = document.getElementById("form");
-    
-    let operandA = form['operandA'];
-    let operandB = form['operandB'];
-    
-    let resultAdd = parseInt(operandA.value) / parseInt(operandB.value);
-    if (isNaN(resultAdd)) {
-        resultAdd = "The operation does not include numbers."
-    } 
-    document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
+  const form = document.getElementById("form");
+
+  let operandA = form["operandA"];
+  let operandB = form["operandB"];
+
+  let resultAdd = parseInt(operandA.value) / parseInt(operandB.value);
+  if (isNaN(resultAdd)) {
+    resultAdd = "The operation does not include numbers.";
+  }
+  document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
 }
 
 function exp() {
-    const form = document.getElementById("form");
-    
-    let operandA = form['operandA'];
-    let operandB = form['operandB'];
-    
-    let resultAdd = parseInt(operandA.value) ** parseInt(operandB.value);
-    if (isNaN(resultAdd)) {
-        resultAdd = "The operation does not include numbers."
-    } 
-    document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
+  const form = document.getElementById("form");
+
+  let operandA = form["operandA"];
+  let operandB = form["operandB"];
+
+  let resultAdd = parseInt(operandA.value) ** parseInt(operandB.value);
+  if (isNaN(resultAdd)) {
+    resultAdd = "The operation does not include numbers.";
+  }
+  document.getElementById("result").innerHTML = `Result: ${resultAdd}`;
 }
 
+function clear() {
+  
+}
